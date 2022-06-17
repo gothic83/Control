4d5b2b785cd7a260a41d215cd37def1aaf7bcc6a
Dmitry <gothic83@example.com>
2022-06-17 16:04:35 +0400

    Додавлена блок схема и решение задачи

---
+++ Zadanie/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] words = { "абр", "123", "хурма", "дом", "пенал", "игорь", "ААА", "ура", "yandex", "почка" };
+string[] newArray = new string[words.Length];
+void SecondArray(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            newArray[j] = array[i];
+        j++;
+    }
+}
+void ShowArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+SecondArray(words);
+ShowArray(newArray);
